12beb4ded96967342582a599ecc6f889e04078fc
Kyle Polich <kylepolich@gmail.com>
2014-05-25 10:58:19 -0700

    format episodes based on libsyn feed

--- www/episodes.php
+++ www/episodes.php
@@ -1,9 +1,50 @@
 <? include("header.php"); ?>
+<?
+  try {
+    $xml = new XMLReader();
+    $xml->open('feed.rss');
+    $item = false;
+    $posts = array();
+    while ($xml->read()) {
+      if($xml->nodeType==XMLReader::ELEMENT) {
+        while($xml->read()) {
+          $name = $xml->name;
+          if($xml->nodeType==XMLReader::ELEMENT) {
+            $xml->read();
+            $value = $xml->value;
+            if (strcmp($name, "item") == 0) {
+              $item = true;
+              $post = array();
+            }
+            else if ($item) {
+              $post[$name] = $value;
+            }
+          }
+          if($xml->nodeType==XMLReader::END_ELEMENT && strcmp($name, "item") == 0) {
+            if ($item) {
+              array_push($posts, $post);
+               $item = false;
+            }
+          }
+        }
+      }
+    }
+  }
+  catch (Exception $e) {
+    echo($e);
+  }
+?>
 
 <div id="bbody">
-<p>
-On libsyn soon
-</p>
+<?
+  foreach ($posts as $post) {
+    echo("<h2>" . $post['title'] . "</h2>");
+    $posixTime = strtotime($post['pubDate']);
+    echo("<b>Posted:</b> " . date('Y-m-d', $posixTime) . "<br/>");
+    echo("<b>Duration:</b> " . $post['itunes:duration'] . "<br/>");
+    echo("<p>" . $post['description'] . "</p>");
+  }
+?>
 </div>
 
 <? include("footer.php"); ?>
