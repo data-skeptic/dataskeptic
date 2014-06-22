ba8c3d8abeaa38c96243939417be5359fb8a0bcb
Kyle Polich <kylepolich@gmail.com>
2014-06-22 08:40:42 -0700

    expanding description

--- www/index.php
+++ www/index.php
@@ -9,6 +9,15 @@ of applying critical thinking and the scientific method to
 evaluate the veracity of claims and efficacy of approaches.
 </p>
 
+<p>The Data Skeptic Podcast has an alternating weekly format.  
+Oddly numbered episodes are short and feature primer discussions about 
+useful topics in data science.  Even episodes are longer conversations 
+with working professionals and researchers doing noteworthy things
+with data.<p>
+
+<p>If you'd like to subscribe to the podcast, please do so on iTunes,
+Zune, Podcast Republic, or whatever your preferred method is.</p>
+
 <p>If you would like to help improve this ugly site or ugly logo, the src
 is available on github and pull requests are warmly considered.</p>
 
