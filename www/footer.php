50a93e403111a1bc0c5982fc02b6b82d5e945f8e
Kyle Polich <kylepolich@gmail.com>
2014-07-27 21:59:41 -0700

    adding speakpipe widget

--- www/footer.php
+++ www/footer.php
@@ -1,17 +1,23 @@
-<br/>
-<table><tr>
-<td><a href="http://dataskeptic.libsyn.com/rss"><img src="/rss2.gif" border=0" /></a></td>
-<td width=50> </td>
-<td><a href="http://feedvalidator.org/check.cgi?url=http%3A//dataskeptic.libsyn.com/rss">
-<img src="/valid-rss.png" />
-</a></td>
-<td width=50> </td>
-<td><a href="https://itunes.apple.com/us/podcast/the-data-skeptic-podcast/id890348705">
-<img src="/itunes.png" height=35 /> on iTunes
-</a></td>
-</tr></table>
+<br/> <table><tr> <td><a href="http://dataskeptic.libsyn.com/rss"><img src="/rss2.gif" border=0" /></a></td> <td width=50> </td> <td><a 
+href="http://feedvalidator.org/check.cgi?url=http%3A//dataskeptic.libsyn.com/rss"> <img src="/valid-rss.png" /> </a></td> <td width=50> </td> <td><a 
+href="https://itunes.apple.com/us/podcast/the-data-skeptic-podcast/id890348705"> <img src="/itunes.png" height=35 /> on iTunes </a></td> <td width=50> </a> <td> <a 
+href="http://www.stitcher.com/s?fid=50561&refid=stpr"><img src="http://cloudfront.assets.stitcher.com/promo.assets/stitcher-banner-120x90.jpg" width="120" height="90" 
+alt="Listen to Stitcher"></a> </td> </tr></table>
 
 </center>
+
+<div id="speakpipe">
+<!-- Begin SpeakPipe code -->
+<script type="text/javascript">
+(function(d){
+var app = d.createElement('script'); app.type = 'text/javascript'; app.async = true;
+var pt = ('https:' == document.location.protocol ? 'https://' : 'http://');
+app.src = pt + 'www.speakpipe.com/loader/1m0jjfmz48oamyjh1jsy3qip1z1vbu12.js'; 
+var s = d.getElementsByTagName('script')[0]; s.parentNode.insertBefore(app, s);
+})(document);
+</script>
+<!-- End SpeakPipe code -->
+</div>
 
 <script>
   (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
