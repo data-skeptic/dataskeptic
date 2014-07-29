d6c1baf24ed3a884426e1b71f05c9cada949b1fa
Kyle Polich <kylepolich@gmail.com>
2014-07-28 20:19:35 -0700

    more ui tweaks

--- www/header.php
+++ www/header.php
@@ -6,11 +6,16 @@
 
 <div id="main">
 <div id="header">
-<img src="/ituneslogo.png" alt="Data Skeptic Podcast" text="Data Skeptic Podcast" />
+<table><tr>
+<td valign=top><img src="/ituneslogo.png" alt="Data Skeptic Podcast" text="Data Skeptic Podcast" /></td>
+<td>
+<a href="https://itunes.apple.com/us/podcast/the-data-skeptic-podcast/id890348705"> <img src="/itunes.png" height=35 /> on iTunes </a> <br/><br/>
+<a href="http://www.stitcher.com/s?fid=50561&refid=stpr"><img src="http://cloudfront.assets.stitcher.com/promo.assets/stitcher-banner-120x90.jpg" width="120" height="90" alt="Listen to Stitcher"></a> <br/>
+</td>
+</tr></table>
 
-<br/>
-<br/>
 
+<div id="menu">
 <a href="/">Home</a>
 |
 <a href="/episodes.php">Show Notes</a>
@@ -22,7 +27,6 @@
 <a href="/resources.php">Resources</a>
 |
 <a href="/contact.php">Contact</a>
+</div>
 
-<br/>
-<br/>
 </div>
