d325b02e585182a9c1bcd1178ea595c006b4ba5d
Kyle Polich <kylepolich@gmail.com>
2014-09-20 08:58:36 -0700

    site improvements

--- www/header.php
+++ www/header.php
@@ -1,19 +1,27 @@
 <HTML>
+<head>
+  <title>Data Skeptic | A podcast about data science and skepticism</title>
+  <meta name="Keywords" content="data science, data skepticism, kyle polich, empirical evidence">
+  <meta name="Description" content="The Data Skeptic Podcast features conversations on topics related to data science, statistics, machine learning, artificial intelligence and the like, all from the perspective of applying critical thinking and the scientific method to evaluate the veracity of claims and efficacy of approaches.">
+<link rel="apple-touch-icon" sizes="57x57" href="/icon/apple-touch-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="114x114" href="/icon/apple-touch-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="72x72" href="/icon/apple-touch-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="144x144" href="/icon/apple-touch-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="60x60" href="/icon/apple-touch-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="120x120" href="/icon/apple-touch-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="76x76" href="/icon/apple-touch-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="152x152" href="/icon/apple-touch-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="/icon/apple-touch-icon-180x180.png">
+<link rel="icon" type="image/png" href="/icon/favicon-192x192.png" sizes="192x192">
+<link rel="icon" type="image/png" href="/icon/favicon-160x160.png" sizes="160x160">
+<link rel="icon" type="image/png" href="/icon/favicon-96x96.png" sizes="96x96">
+<link rel="icon" type="image/png" href="/icon/favicon-16x16.png" sizes="16x16">
+<link rel="icon" type="image/png" href="/icon/favicon-32x32.png" sizes="32x32">
+<meta name="msapplication-TileColor" content="#da532c">
+<meta name="msapplication-TileImage" content="/icon/mstile-144x144.png">
+<link rel="stylesheet" type="text/css" href="style.css" media="screen" />
+</head>
 <BODY>
-
-<style>
-h1 {
-  color: 44AA44
-}
-p {
-  color: 224422
-}
-#bbody {
-  background-color:#ccc;
-  width: 600px;
-  padding: 10px;
-  text-align: left;
-}
 
 </style>
 <center>
