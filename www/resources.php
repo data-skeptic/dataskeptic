de194225544bd0a25489a9e5d8a1f1a1741975b9
Kyle Polich <kylepolich@gmail.com>
2014-09-21 17:39:48 -0700

    some missing commits

--- www/resources.php
+++ www/resources.php
@@ -2,18 +2,18 @@
 
 <div id="bbody">
 
-<table>
+<table cellpadding=5>
 <tr>
-  <td align=top><a href="http://www.openintro.org/stat/">OpenIntro</a></td>
-  <td>OpenIntro provides a great introduction to statistics textbook as a FREE eBook or an at-cost print version.  I got one for under $10 on amazon.  This is a great resource for any listener without a statistics background.</td>
+  <td valign=top><a href="http://www.openintro.org/stat/">OpenIntro</a></td>
+  <td valign=top>OpenIntro provides a great introduction to statistics textbook as a FREE eBook or an at-cost print version.  I got one for under $10 on amazon.  This is a great resource for any listener without a statistics background.</td>
 </tr>
 <tr>
-  <td align=top><a href="http://www.skepdic.com/">Skeptic's Dictionary</a></td>
-  <td>A good reference to skeptical materials and references on many questionable claims.</td>
+  <td valign=top><a href="http://www.skepdic.com/">Skeptic's Dictionary</a></td>
+  <td valign=top>A good reference to skeptical materials and references on many questionable claims.</td>
 </tr>
 <tr>
-  <td align=top><a href="http://whatstheharm.net/">What's the Harm?</a></td>
-  <td>When presented with counter-arguments or empirical falsifications of claims, some people often respond with "What's the Harm?".  This site is a great place to answer that question.</td>
+  <td valign=top><a href="http://whatstheharm.net/">What's the Harm?</a></td>
+  <td valign=top>When presented with counter-arguments or empirical falsifications of claims, some people often respond with "What's the Harm?".  This site is a great place to answer that question.</td>
 </tr>
 
 </table>
