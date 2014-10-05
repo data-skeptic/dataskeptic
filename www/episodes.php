1d417ad9ddd73947e1123ca5c51281e89c1cdcec
Kyle Polich <kylepolich@gmail.com>
2014-10-04 22:12:11 -0700

    update player for episodes page

--- www/episodes.php
+++ www/episodes.php
@@ -56,9 +56,16 @@
 ?>
 
 <div id="bbody">
+<h2>Recent Episodes</h2>
 <?
+$showonce=0;
   $i = count($posts);
+  $max = 6;
   foreach ($posts as $post) {
+    if ($max <=0) {
+      continue;
+    }
+    $max--;
     echo("<div class='ep'>");
     $link = $post['link'];
     if (strpos($link, '/epnotes/') !== FALSE) {
@@ -69,22 +76,68 @@
     }
     $i -= 1;
     $posixTime = strtotime($post['pubDate']);
-    echo("<table><tr><td>");
     echo("<b>Posted:</b> " . date('Y-m-d', $posixTime) . "<br/>");
     echo("<b>Duration:</b> " . $post['itunes:duration'] . "<br/>");
-    echo("</td><td>");
     $aid = $post['aid'];
-    if ($aid != "") {
-      echo("<iframe style=\"border: none\" src=\"//html5-player.libsyn.com/embed/episode/id/" . $aid . "/height/360/width/640/theme/standard/direction/no/autoplay/no/autonext/no/thumbnail/yes/preload/no/no_addthis/no/\" height=\"125\" width=\"500\" scrolling=\"no\"  allowfullscreen webkitallowfullscreen mozallowfullscreen oallowfullscreen msallowfullscreen></iframe>");
-    }
-    else {
-      echo("<p><audio controls='controls' style='width: 300px'><source type='" . $post['a_type'] . "' src='" . $post['a_url'] . "' />Your browser does not support our audio player.</audio></p>");
-    }
-    echo("</td></tr></table>");
-    echo("<p>" . $post['description'] . "</p>");
+if ($showonce==0) {
+$showonce=0;
+?>
+<div id="jquery_jplayer_<? echo($i); ?>" class="jp-jplayer"></div>
+
+<div id="jp_container_<? echo($i); ?>" class="jp-audio">
+    <div class="jp-type-single">
+        <div class="jp-gui jp-interface">
+            <ul class="jp-controls">
+                <li><a href="javascript:;" class="jp-play" tabindex="1">play</a></li>
+                <li><a href="javascript:;" class="jp-pause" tabindex="1">pause</a></li>
+                <li><a href="javascript:;" class="jp-volume-max" tabindex="1" title="max volume">max volume</a></li>
+            </ul>
+            <div class="jp-progress">
+                <div class="jp-seek-bar">
+                    <div class="jp-play-bar"></div>
+                </div>
+            </div>
+            <div class="jp-volume-bar">
+                <div class="jp-volume-bar-value"></div>
+            </div>
+            <div class="jp-current-time"></div>
+            <div class="jp-duration"></div>
+        </div>
+        <div class="jp-details">
+            <ul>
+                <li><span class="jp-title"></span></li>
+            </ul>
+        </div>
+        <div class="jp-no-solution">
+            <span>Update Required</span>
+            To play the media you will need to either update your browser to a recent version to use our player
+        </div>
+    </div>
+</div>
+<script>
+$(document).ready(function() {
+
+    $("#jquery_jplayer_<? echo($i); ?>").jPlayer({
+        ready: function(event) {
+            $(this).jPlayer("setMedia", {
+                title: "<? echo($post['title']); ?>",
+                mp3: "<? echo($post['a_url']); ?>"
+            });
+        },
+        swfPath: "http://jplayer.org/latest/js",
+        cssSelectorAncestor: '#jp_container_<? echo($i); ?>',
+        supplied: "mp3"
+    });
+});
+</script>
+
+<?
+}
+    echo("" . $post['description'] . "");
     echo("</div><br/>");
   }
 ?>
+For a complete list, please find the podcast on itunes, stitcher, or your podcasting place of choice.
 </div>
 
 <? include("footer.php"); ?>
