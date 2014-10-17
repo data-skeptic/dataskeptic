ffd8f6acab7fe3c8fa15525a4b9069465d3e27f9
Kyle Polich <kylepolich@gmail.com>
2014-10-17 16:42:06 -0700

    revise home page

--- www/index.php
+++ www/index.php
@@ -9,17 +9,20 @@ of applying critical thinking and the scientific method to
 evaluate the veracity of claims and efficacy of approaches.
 </p>
 
-<p>The Data Skeptic Podcast has an alternating weekly format.  
-Oddly numbered episodes are short and feature primer discussions about 
-useful topics in data science.  Even episodes are longer conversations 
-with working professionals and researchers doing noteworthy things
+<p>The Data Skeptic Podcast has an alternating weekly format. 
+Episodes are either "mini" episodes, lasting 10-15 minutes 
+featuring a high level discussions with co-host Linhda (a 
+non-data scientist) on useful topics of data science.  These 
+make great primers for non-technical people.</p>
+
+<p>Other eisodes feature longer interviews with 
+working professionals and researchers doing noteworthy things
 with data.<p>
 
 <p>If you'd like to subscribe to the podcast, please do so on iTunes,
-Sticher, Zune, Podcast Republic, or whatever your preferred method is.</p>
-
-<p>If you would like to help improve this ugly site or ugly logo, the src
-is available on github and pull requests are warmly considered.</p>
+Sticher, Zune, Podcast Republic, or whatever your preferred method is.
+You can also listen directly from the site by visiting each 
+episode's <a href="episodes.php">Show Notes</a> page.</p>
 
 </div>
 
