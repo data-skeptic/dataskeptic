7abfa504a01bbe369f04e4bc8bb8d50d5f504fa4
Kyle Polich <kylepolich@gmail.com>
2014-12-01 14:16:00 -0800

    update contact page

--- www/contact.php
+++ www/contact.php
@@ -1,15 +1,13 @@
 <? include("header.php"); ?>
 
 <div id="bbody">
+<p><i>Have a question for the show or feedback on an episode?</i> Please ask us via Facebook or Twitter so that everyone can benefit from the discussion.</p>
+
 <p>The official twitter account of the show is <a href="https://twitter.com/dataskeptic">@dataskeptic</a></p>
 
 <p>Please follow our Facebook page <a href="https://www.facebook.com/dataskeptic">Data Skeptic</a></p>
 
-<p>To reach the show via email, contact <a href="mailto:kylepolich@gmail.com">kylepolich@gmail.com</a></p>
-
 <p>For show note corrections and similar contributions, pull requests to <a href="http://dataskeptic.com/github.php">Data Skeptic github repository</a> are warmly welcome.</p>
-
-<p>If you have a show related question, please reach out on Twitter or Facebook where everyone can benefit.</p>
 
 </div>
 
