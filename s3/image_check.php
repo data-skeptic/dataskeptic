615c0fc7823e79383f8bf7394787be5922f68e65
Kyle Polich <kylepolich@gmail.com>
2015-04-12 11:20:26 -0700

    s

---
+++ s3/image_check.php
@@ -0,0 +1,12 @@
+<?php
+function getExtension($str) 
+{
+$i = strrpos($str,".");
+if (!$i) { return ""; }
+$l = strlen($str) - $i;
+$ext = substr($str,$i+1,$l);
+return $ext;
+}
+//Here you can add valid file extensions. 
+$valid_formats = array("jpg", "png", "gif", "bmp","jpeg","PNG","JPG","JPEG","GIF","BMP");
+?>
