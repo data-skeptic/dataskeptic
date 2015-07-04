829978d1e3e2c4f61fb4874139fd594de00c60e7
Kyle Polich <kyle@datascience.com>
2015-07-04 13:47:17 -0700

    renaming

---
+++ www/bf/bf.php
@@ -0,0 +1,259 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="utf-8">
+		<title>Sasquatch Footprints — How big are they?</title>
+
+		<!-- jQuery -->
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+		<script type="text/javascript" src="jquery.ui.core.min.js"></script>
+		<script type="text/javascript" src="jquery.ui.widget.min.js"></script>
+		<script type="text/javascript" src="jquery.ui.mouse.min.js"></script>
+		<script type="text/javascript" src="jquery.ui.draggable.min.js"></script>
+
+		<!-- wColorPicker -->
+		<link rel="Stylesheet" type="text/css" href="wColorPicker.css" />
+		<script type="text/javascript" src="wColorPicker.js"></script>
+
+		<!-- wPaint -->
+		<link rel="Stylesheet" type="text/css" href="wPaint.css" />
+		<script type="text/javascript" src="wPaint.js"></script>
+
+		<!--  STYLE SHEETS  -->
+		<link rel="stylesheet" type="text/css" href="bfStyles.css" />
+	</head>
+	
+	<body>
+		<div id="wrapper">
+			<div id="content" class="cf">
+				<header>
+					<a href="/" alt="The Data Skeptic Podcast"><img src="DSlogo_white.png"></a>
+					<h1>Crypto</h1>
+				</header>
+				<p>How do people think rationally about small probability events?</p>
+
+				<div id="menu">
+				intrapage links
+				</div>
+
+				<div id='mmain'>
+
+					<h2>Special Guests</h2>
+					<span class='wbcktxt'>In pursuit of the answer to my research question, I enlisted the help of a number of former guests of the show and other noteworthy experts.  A short bio on each panelist is below in order of appearance.</span>
+					You found a secret message!
+
+
+					<h3>Daniel Loxton</h3>
+					<span class="wbcktxt"><a href="http://www.danielloxton.com/">Daniel Loxton</a> is an author, illustrator, and skeptic.  He's the editor of junior skeptic magazine and author and artist on a number of books including <a href="http://www.amazon.com/Abominable-Science-Origins-Nessie-Cryptids/dp/023115321X/ref=sr_1_5?ie=UTF8&qid=1435971975&sr=8-5&keywords=daniel+loxton">Abominable Science!</a> which we discuss in this episode.  The figure below describes the distribution of images in this beautifully illustrated book.</span>
+					<br/><br/>
+					<table>
+						<tr><td valign=top><img src='daniel.jpg' /></td>
+							<td><img src='guest-visuals/daniel/daniel.png' />
+								<p><a href="https://github.com/kylepolich/dataskeptic/blob/master/www/bf/guest-visuals/daniel/daniel-loxton.ipynb">[src]</a></p>
+							</td>
+						</tr>
+					</table>
+
+
+
+					<h3>Noelle Sio Saldana</h3>
+					<span class="wbcktxt"><a href="https://www.linkedin.com/in/noellesio">Noelle Sio Saldana</a> previously appeared in episode 53 discussing her work with <a href="http://dataskeptic.com/epnotes/ep53_using-data-to-help-those-in-crisis.php">Crisis Text Line</a>.  She also has a degree in physical anthropology, so I asked her about bigfoot.  The figure below summarizes the frequency of her tweets by hour of day and day of week.
+					</span>
+					<br/><br/>
+					<table>
+						<tr><td valign=top><img src='noelle.jpg' /></td>
+							<td><img src='guest-visuals/noelle/noelle.png' />
+								<p><a href="https://github.com/kylepolich/dataskeptic/blob/master/www/bf/guest-visuals/noelle/noelle-saldana.ipynb">[src]</a></p>
+							</td>
+						</tr>
+					</table>
+
+
+
+					<h3>Nathon Janos</h3>
+					<span class="wbcktxt"><a href="https://www.linkedin.com/pub/nathan-janos/0/902/895">Nathan Janos</a> was gratious enough to be the very first interview guest way back in <a href="http://dataskeptic.com/epnotes/ep002.php">episode 2</a> to discuss his work doing advertiser attribution.  The figure below describes the number of daily downloads of that episode.
+					</span>
+					<br/><br/>
+					<table>
+						<tr><td valign=top><img src='nathan.jpg' /></td>
+							<td><img src='guest-visuals/nathan/nathan.png' />
+								<p><a href="https://github.com/kylepolich/dataskeptic/blob/master/www/bf/guest-visuals/nathan/nathan-janos.ipynb">[src]</a></p>
+							</td>
+						</tr>
+					</table>
+
+
+
+					<h3>Richard Saunders</h3>
+					<span class="wbcktxt"><a href="https://en.wikipedia.org/wiki/Richard_Saunders_(skeptic)">Richard Saunders</a> has a long list of skeptical accolades including hosting <a href="http://skepticzone.tv/">The Skeptics Zone</a> podcast, in which he frequently reports his post-intro snack choice.  The figure below summarizes the presumed calorie count of these snacks.</span>
+					<br/><br/>
+					<table>
+						<tr><td valign=top><img src='richard.jpg' /></td>
+							<td><? include('guest-visuals/richard/richard.php'); ?>
+								<p>
+										<a href="https://github.com/kylepolich/dataskeptic/blob/master/www/bf/guest-visuals/richard/richard-saunders.ipynb">[src]</a>
+										<a href="guest-visuals/richard/richard.png">[static img]</a>
+								</p>
+							</td>
+						</tr>
+					</table>
+
+
+
+					<h3>Sharon Hill</h3>
+					<span class="wbcktxt"><a href="https://en.wikipedia.org/wiki/Sharon_A._Hill">Sharon Hill</a> appeared in <a href="http://dataskeptic.com/epnotes/ep35_doubtful-news-geology-and-thinking-scientifically-with-Sharon-Hill.php">episode 35</a> and has a number of other noteworthy skeptical works including most notably, being the editor of <a href="http://doubtfulnews.com/">DoubtfulNews.com</a>.  The plot below summarizes the number of posts on Doubtful News by category and also reports the average number of comments.
+					</span>
+					<br/><br/>
+					<table>
+						<tr><td valign='top'><img src='sharon.jpg' /></td>
+							<td><img src='guest-visuals/sharon/sharon.png' />
+								<p><a href="https://github.com/kylepolich/dataskeptic/blob/master/www/bf/guest-visuals/sharon/sharon-hill.ipynb">[src]</a></p>
+							</td>
+						</tr>
+					</table>
+
+
+
+					<h3>Susan Gerbic</h3>
+					<span class="wbcktxt"><a href="https://about.me/susangerbic">Susan Gerbic</a> founded <a href="http://guerrillaskepticismonwikipedia.blogspot.com/">Guerrilla Skepticism on Wikipedia</a> and appeared on <a href="http://dataskeptic.com/epnotes/ep012.php">episode 12</a> to discuss how the GSoW team improves Wikipedia.  The figure below describes the cumulative time series of edits made by Susan since her Wikipedia account was created.</span>
+					<br/><br/>
+					<table>
+						<tr><td valign=top><img src='susan.jpg' /></td>
+							<td><img src='guest-visuals/susan/susan.png' />
+								<p><a href="https://github.com/kylepolich/dataskeptic/blob/master/www/bf/guest-visuals/susan/susan-gerbic.ipynb">[src]</a></p>
+							</td>
+						</tr>
+					</table>
+
+
+
+					<h3>Lieven Devreese</h3>
+					<span class="wbcktxt"><a href=" "> </a>  </span>
+					<table>
+						<tr><td valign=top><img src='lieven.jpg' /></td>
+							<td><img src='guest-visuals/lieven/lieven.png' />
+								<p><a href="https://github.com/kylepolich/dataskeptic/blob/master/www/bf/guest-visuals/lieven/lieven-devreese.ipynb">[src]</a></p>
+							</td>
+						</tr>
+					</table>
+
+
+
+					<h3>Alex Boklin</h3>
+					<span class="wbcktxt">A
+					http://dataskeptic.com/epnotes/ep29_the-secret-and-the-global-consciousness-project.php
+					</span>
+					<table>
+						<tr><td valign=top><img src='alex.jpg' /></td>
+							<td><img src='guest-visuals/alex/alex.png' />
+								<p><a href="https://github.com/kylepolich/dataskeptic/blob/master/www/bf/guest-visuals/alex/alex-boklin.ipynb">[src]</a></p>
+							</td>
+						</tr>
+					</table>
+
+
+
+					<h3>Karl Mamer</h3>
+					<span class="wbcktxt"><a href="http://www.yrad.com/cs/">Karl Mamer</a> hosts The Conspiracy Skeptic on which Kyle appear in 2014, <a href="http://askacanadian.org/">Ask a Canadian</a>, as well as appearing on previous Data Skeptic episode <a href="http://dataskeptic.com/epnotes/ep023.php">Urban Legends</a>.  The figure below is a histogram of episode length for The Conspiracy Skeptic Podcast.</span>
+					<br/><br/>
+					<table>
+						<tr><td valign=top><img src='karl.jpg' /></td>
+							<td><img src='guest-visuals/karl/karl.png' />
+								<p><a href="https://github.com/kylepolich/dataskeptic/blob/master/www/bf/guest-visuals/karl/karl-mamer.ipynb">[src]</a></p>
+							</td>
+						</tr>
+					</table>
+				</div>
+			</div>
+		</div>
+!<--alex	daniel	karl	lieven	nathan	noelle	richard	sharon	susan-->
+		<footer>
+			<ul>
+				<li><a href="/">DataSkeptic Podcast</a></li>
+				<li><a href="../episodes.php">Show Notes</a></li>
+				<li><a href="../blog.php">Blog</a></li>
+				<li><a href="../resources.php">Resources</a></li>
+				<li><a href="../contact.php">Contact</a></li>
+			</ul>
+		</footer>
+		<script type="text/javascript">
+			function dataURItoBlob(dataURI) {
+			    // convert base64/URLEncoded data component to raw binary data held in a string
+			    var byteString;
+			    if (dataURI.split(',')[0].indexOf('base64') >= 0)
+			        byteString = atob(dataURI.split(',')[1]);
+			    else
+			        byteString = unescape(dataURI.split(',')[1]);
+
+			    // separate out the mime component
+			    var mimeString = dataURI.split(',')[0].split(':')[1].split(';')[0];
+
+			    // write the bytes of the string to a typed array
+			    var ia = new Uint8Array(byteString.length);
+			    for (var i = 0; i < byteString.length; i++) {
+			        ia[i] = byteString.charCodeAt(i);
+			    }
+			    return new Blob([ia], {type:mimeString});
+			}
+
+
+			var wp = $("#wPaint").wPaint({
+				strokeStyle: '#000000',
+				drawDown: function(e, mode){ $("#canvasDown").val(this.settings.mode + ": " + e.pageX + ',' + e.pageY); },
+				drawMove: function(e, mode){ $("#canvasMove").val(this.settings.mode + ": " + e.pageX + ',' + e.pageY); },
+				drawUp: function(e, mode){ $("#canvasUp").val(this.settings.mode + ": " + e.pageX + ',' + e.pageY); }
+			}).data('_wPaint');
+				
+			function saveImage() {
+				var imageData = $("#wPaint").wPaint("image");
+				$("#canvasImage").attr('src', imageData);
+				$("#canvasImageData").val(imageData);
+			}
+
+			function clearCanvas() {
+				$("#wPaint").wPaint("clear");
+			}
+				
+			function upload_image(id) {
+				var imageData = $("#" + id).wPaint("image");
+				var email = $('#email').val();
+				var prob = $('#prob').val();
+				var blob = dataURItoBlob(imageData);
+				var fd = new FormData();
+				fd.append('file', blob);
+				$.ajax({
+					url: 'hello.py/post_img?email=' + email + "&prob=" + prob,
+					type: 'POST',
+					data: fd,
+					async: false,
+					cache: false,
+					enctype: 'multipart/form-data',
+					processData: false,
+					contentType: false,
+					success: function(data) {
+						alert('Thanks for participating');
+						clearCanvas();
+					}
+				});
+			}
+
+			$.extend($.fn.wPaint.defaults, {
+			  mode:        'pencil',  // set mode
+			  lineWidth:   '3',       // starting line width
+			  fillStyle:   '#FFFFFF', // starting fill style
+			  strokeStyle: '#000000'  // start stroke style
+			});
+		</script>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-51062432-1', 'dataskeptic.com');
+  ga('send', 'pageview');
+
+</script>
+
+	</body>
+</html>
