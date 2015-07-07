ab942b7115cdd3fc11bc81d3f9d792dfc3a4ce08
Kyle Polich <kylepolich@gmail.com>
2015-07-06 23:43:25 -0700

    remove miro

--- www/header.php
+++ www/header.php
@@ -72,8 +72,6 @@
       <a href="https://itunes.apple.com/us/podcast/the-data-skeptic-podcast/id890348705"><img src="/itunes.png" height=35 alt="Data Science itunes" /></a>
       &nbsp;
       <a href="http://www.stitcher.com/s?fid=50561&refid=stpr"><img src="/stitcher_234x60.jpg" alt="Data Science Stitcher" height=35></a>
-      &nbsp;
-      <a href="http://subscribe.getmiro.com/?type=audio&url1=http%3A%2F%2Fpodcast.datadrivensecurity.info%2Ffeed%2Fddsec-feed.xml" alt="Miro"><img src="/Miro-icon.png" height=35 /></a>
     </div>
   </div>
   <div class="clear"></div>
