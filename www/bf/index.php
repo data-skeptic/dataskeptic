fa200919d70c57cacaf8b2cfe95171016f1efedd
Kyle Polich <kyle@datascience.com>
2015-07-14 08:26:49 -0700

    mv

--- www/bf/index.php
+++ www/bf/index.php
@@ -1,15 +1,3 @@
-<?
-function generateRandomString($length = 10) {
-    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
-    $charactersLength = strlen($characters);
-    $randomString = '';
-    for ($i = 0; $i < $length; $i++) {
-        $randomString .= $characters[rand(0, $charactersLength - 1)];
-    }
-    return $randomString;
-}
-$code = generateRandomString(6);
-?>
 <!DOCTYPE html>
 <html lang="en">
 	<head>
@@ -40,48 +28,180 @@ $code = generateRandomString(6);
 			<div id="content" class="cf">
 				<header>
 					<a href="/" alt="The Data Skeptic Podcast"><img src="DSlogo_white.png"></a>
-					<h1>Sasquatch Footprints</h1>
+					<h1>Crypto</h1>
 				</header>
-				<p>Whether he is called Sasquatch, Bigfoot, or any of a wide variety of names, the topic of Bigfoot is controversial &mdash; some have no doubt these creatures exists, others do not see enough evidence. Regardless of where you stand, one cannot deny that Bigfoot has had a significant place in our folklore and popular culture. </p>
+				<p>How do people think rationally about small probability events?</p>
 
-				<p>We&#39;d like your help testing what people think is the average size of Bigfoot&#39;s footprint, relative to an average human&#39;s foot. Please draw a footprint in the space below that reflects the size you think of for a bigfoot's print.</p>
+				<span style='font-size: 24pt'>This page is NOT yet ready for sharing!!!  But it is the permalink that will be live on July 17th (the official release date of the episode)</span>
 
 				<div id="menu">
+				intrapage links go here
+				</div>
+
+				<br/>
+
+				<div id='mmain'>
+
+					<h2>Special Guests</h2>
+					<span class='wbcktxt'>In pursuit of the answer to my research question, I enlisted the help of a number of former guests of the show and other noteworthy experts.  A short bio on each panelist is below in order of appearance.</span>
+					You found a secret message!
+
+
+					<h3>Daniel Loxton</h3>
+					<span class="wbcktxt"><a href="http://www.danielloxton.com/">Daniel Loxton</a> is an author, illustrator, and skeptic.  He's the editor of <a href="http://www.skeptic.com/junior_skeptic/">Junior Skeptic</a> Magazine and author and artist on a number of books including <a href="http://www.amazon.com/Abominable-Science-Origins-Nessie-Cryptids/dp/023115321X/ref=sr_1_5?ie=UTF8&qid=1435971975&sr=8-5&keywords=daniel+loxton">Abominable Science!</a> which we discuss in this episode.  The figure below describes the distribution of images in this beautifully illustrated book.</span>
+					<br/><br/>
 					<table>
-						<tr>
-							<td align="center">
-								<div id="wPaint"></div>
-							</td>
-							<td>
-								<img id="canvasImage" src=""/>
+						<tr><td valign=top><img src='daniel.jpg' /></td>
+							<td><!--? include('guest-visuals/daniel/daniel.php'); ?-->
+								<img src='guest-visuals/daniel/daniel.png' />
+								<p><a href="https://github.com/kylepolich/dataskeptic/blob/master/www/bf/guest-visuals/daniel/daniel-loxton.ipynb">[src]</a>
+								<!--<a href='guest-visuals/daniel/daniel.png'>[static img]</a>--></p>
 							</td>
 						</tr>
 					</table>
-				</div>	
-				<div>
-					<center>
-					<br/>
+
+
+
+					<h3>Noelle Sio Saldana</h3>
+					<span class="wbcktxt"><a href="https://www.linkedin.com/in/noellesio">Noelle Sio Saldana</a> previously appeared in episode 53 discussing her work with <a href="http://dataskeptic.com/epnotes/ep53_using-data-to-help-those-in-crisis.php">Crisis Text Line</a>.  She also has a degree in physical anthropology, so I asked her about bigfoot.  The figure below summarizes the frequency of her tweets by hour of day and day of week.
+					</span>
+					<br/><br/>
 					<table>
-					<tr>
-					<td>This field contains a test code, take note if you come from Mechanical Turk: &nbsp; </td>
-					<td><input id='code' style='width: 100px; font-size: 14pt' value='<? echo($code); ?>' /></td>
-					</tr>
-					</tr><tr>
-					<td>What is your email? (optional)</td>
-					<td><input id='email' style='width:250px; font-size: 14pt' /></td>
-					</tr><tr>
-					<td>With what probability (0.00 - 100.0%) do you think Bigfoot might be real? &nbsp; </td>
-					<td><input id='prob' style='width: 100px; font-size: 14pt' /> % </td>
-					</tr>
+						<tr><td valign=top><img src='noelle.jpg' /></td>
+							<td><!--? include('guest-visuals/noelle/noelle.php'); ?-->
+								<img src='guest-visuals/noelle/noelle.png' />
+								<p>
+									<a href="https://github.com/kylepolich/dataskeptic/blob/master/www/bf/guest-visuals/noelle/noelle-saldana.ipynb">[src]</a>
+									<!--<a href='guest-visuals/noelle/noelle.png'>[static img]</a>-->
+								</p>
+							</td>
+						</tr>
 					</table>
-					</center>
-				</div>
-				<div id="buttonHolder">
-					<button class="butBase" id="clearBut" onclick='clearCanvas();'>Clear</button>
-					<input class="butBase" id="subBut" type="button" value="Upload Image" onclick="upload_image('wPaint');"/>
+
+
+
+					<h3>Nathon Janos</h3>
+					<span class="wbcktxt"><a href="https://www.linkedin.com/pub/nathan-janos/0/902/895">Nathan Janos</a> was gratious enough to be the very first interview guest way back in episode #3 to discuss his work doing <a href="http://dataskeptic.com/epnotes/ep002.php">advertiser attribution</a>.  The figure below describes the number of daily downloads of that episode.
+					</span>
+					<br/><br/>
+					<table>
+						<tr><td valign=top><img src='nathan.jpg' /></td>
+							<td><!--? include('guest-visuals/nathan/nathan.php'); ?-->
+								<img src='guest-visuals/nathan/nathan.png' />
+								<p>
+									<a href="https://github.com/kylepolich/dataskeptic/blob/master/www/bf/guest-visuals/nathan/nathan-janos.ipynb">[src]</a>
+									<!--<a href='guest-visuals/nathan/nathan.png'>[static img]</a>-->
+								</p>
+							</td>
+						</tr>
+					</table>
+
+
+
+					<h3>Richard Saunders</h3>
+					<span class="wbcktxt"><a href="https://en.wikipedia.org/wiki/Richard_Saunders_(skeptic)">Richard Saunders</a> has a long list of skeptical accolades including producing the documentary <a href="http://www.skeptics.com.au/latest/news/the-vaccination-chronicles/">The Vacciantion Chronicles</a> as well as hosting <a href="http://skepticzone.tv/">The Skeptics Zone</a> podcast, in which he frequently reports his post-intro snack choice.  The figure below summarizes the presumed calorie count of these snacks; please roll over the bars for details.</span>
+					<br/><br/>
+					<table>
+						<tr><td valign=top><img src='richard.jpg' /></td>
+							<td><? include('guest-visuals/richard/richard.php'); ?>
+								<p>
+										<a href="https://github.com/kylepolich/dataskeptic/blob/master/www/bf/guest-visuals/richard/richard-saunders.ipynb">[src]</a>
+										<a href="guest-visuals/richard/richard.png">[static img]</a>
+								</p>
+							</td>
+						</tr>
+					</table>
+
+
+
+					<h3>Sharon Hill</h3>
+					<span class="wbcktxt"><a href="https://en.wikipedia.org/wiki/Sharon_A._Hill">Sharon Hill</a> appeared in <a href="http://dataskeptic.com/epnotes/ep35_doubtful-news-geology-and-thinking-scientifically-with-Sharon-Hill.php">episode 35</a> and has a number of other noteworthy skeptical works including most notably, being the editor of <a href="http://doubtfulnews.com/">DoubtfulNews.com</a>.  The plot below summarizes the number of posts on Doubtful News by category and also reports the average number of comments.
+					</span>
+					<br/><br/>
+					<table>
+						<tr><td valign='top'><img src='sharon.jpg' /></td>
+							<td><!--? include('guest-visuals/sharon/sharon.php'); ?-->
+								<img src='guest-visuals/sharon/sharon.png' />
+								<p>
+									<a href="https://github.com/kylepolich/dataskeptic/blob/master/www/bf/guest-visuals/sharon/sharon-hill.ipynb">[src]</a>
+									<!--<a href='guest-visuals/sharon/sharon.png'>[static img]</a>-->
+								</p>
+							</td>
+						</tr>
+					</table>
+
+
+
+					<h3>Susan Gerbic</h3>
+					<span class="wbcktxt"><a href="https://about.me/susangerbic">Susan Gerbic</a> founded <a href="http://guerrillaskepticismonwikipedia.blogspot.com/">Guerrilla Skepticism on Wikipedia</a> and appeared on <a href="http://dataskeptic.com/epnotes/ep012.php">episode 12</a> to discuss how the GSoW team improves Wikipedia.  The figure below describes the cumulative time series of edits made by Susan since her Wikipedia account was created.</span>
+					<br/><br/>
+					<table>
+						<tr><td valign=top><img src='susan.jpg' /></td>
+							<td><!--? include('guest-visuals/susan/susan.php'); ?-->
+								<img src='guest-visuals/susan/susan.png' />
+								<p>
+									<a href="https://github.com/kylepolich/dataskeptic/blob/master/www/bf/guest-visuals/susan/susan-gerbic.ipynb">[src]</a>
+									<!--<a href='guest-visuals/susan/susan.png'>[static img]</a>-->
+								</p>
+							</td>
+						</tr>
+					</table>
+
+
+
+					<h3>Lieven Devreese</h3>
+					<span class="wbcktxt"><a href="https://vimeo.com/user4444136">Lieven Devreese</a> is a primatologist who has has studied the agiel mangabeys in Central Africa and earlier this year, embarked on an Indigogo funded campaign to get the first confirmed evidence of the Red Colobus Monkey.  Lieven graciously shared some of the story of his expedition for this episode.</span>
+					<br/><br/>
+					<table>
+						<tr><td valign=top><img src='lieven.jpg' /></td>
+							<td><!--? include('guest-visuals/lieven/lieven.php'); ?-->
+								<img src='guest-visuals/lieven/lieven.png' />
+								<p>
+									<a href="https://github.com/kylepolich/dataskeptic/blob/master/www/bf/guest-visuals/lieven/lieven-devreese.ipynb">[src]</a>
+									<!--<a href='guest-visuals/lieven/lieven.png'>[static img]</a>-->
+								</p>
+							</td>
+						</tr>
+					</table>
+
+
+
+					<h3>Alex Boklin</h3>
+					<span class="wbcktxt"><a href="https://ru.linkedin.com/in/alexboklin">Alex Boklin</a> appeared on episode 29 to share his insight on <a href="http://dataskeptic.com/epnotes/ep29_the-secret-and-the-global-consciousness-project.php">The Secret and the Global Consciousness Project</a>.  Alex wrote a great paper titled <a href="http://gmu.academia.edu/AlexBoklin">The Miracle of Large Numbers</a>.  The figure below describes the top 10 <a href="https://en.wikipedia.org/wiki/Tf%E2%80%93idf">term frequency-inverse document frequency</a> weights calculated for his article when each paragraph is treated as a unique element.
+					</span>
+					<br/><br/>
+					<table>
+						<tr><td valign=top><img src='alex.jpg' /></td>
+							<td><!--? include('guest-visuals/alex/alex.php'); ?-->
+								<img src='guest-visuals/alex/alex.png' />
+								<p>
+									<a href="https://github.com/kylepolich/dataskeptic/blob/master/www/bf/guest-visuals/alex/alex-boklin.ipynb">[src]</a>
+									<!--<a href='guest-visuals/alex/alex.png'>[static img]</a>-->
+								</p>
+							</td>
+						</tr>
+					</table>
+
+
+
+					<h3>Karl Mamer</h3>
+					<span class="wbcktxt"><a href="http://www.yrad.com/cs/">Karl Mamer</a> hosts The Conspiracy Skeptic on which Kyle appear in 2014, <a href="http://askacanadian.org/">Ask a Canadian</a>, as well as appearing on previous Data Skeptic episode <a href="http://dataskeptic.com/epnotes/ep023.php">Urban Legends</a>.  The figure below is a histogram of episode length for The Conspiracy Skeptic Podcast.</span>
+					<br/><br/>
+					<table>
+						<tr><td valign=top><img src='karl.jpg' /></td>
+							<td><!--? include('guest-visuals/karl/karl.php'); ?-->
+								<img src='guest-visuals/karl/karl.png' />
+								<p>
+									<a href="https://github.com/kylepolich/dataskeptic/blob/master/www/bf/guest-visuals/karl/karl-mamer.ipynb">[src]</a>
+									<!--<a href='guest-visuals/karl/karl.png'>[static img]</a>-->
+								</p>
+							</td>
+						</tr>
+					</table>
 				</div>
 			</div>
 		</div>
+!<--alex	daniel	karl	lieven	nathan	noelle	richard	sharon	susan-->
 		<footer>
 			<ul>
 				<li><a href="/">DataSkeptic Podcast</a></li>
