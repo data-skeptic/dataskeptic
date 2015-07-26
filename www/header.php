99a87280d293a23ab0eb7c573d57e6bb46afb252
Kyle Polich <kylepolich@gmail.com>
2015-07-26 09:04:51 -0700

    updates

--- www/header.php
+++ www/header.php
@@ -24,16 +24,35 @@
 	<link rel="stylesheet" type="text/css" href="/skin/jplayer.css" media="screen" />
 	<link rel="stylesheet" type="text/css" href="/style.css" />
 	<link href="http://cdn-images.mailchimp.com/embedcode/classic-081711.css" rel="stylesheet" type="text/css">
+
+<link rel="apple-touch-icon" sizes="57x57" href="/apple-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="/apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="/apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="/apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="/apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="/apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="/apple-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="/apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="/apple-icon-180x180.png">
+<link rel="icon" type="image/png" sizes="192x192"  href="/android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+<link rel="manifest" href="/manifest.json">
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="msapplication-TileImage" content="/ms-icon-144x144.png">
+<meta name="theme-color" content="#ffffff">
 </head>
 <body>
 <div id='left'>
-<img src='/web2.jpg' />
-<br/>
+<img src='/ds-logo.svg' />
 <div id='left2'>
 <center>
 <a href="https://itunes.apple.com/us/podcast/the-data-skeptic-podcast/id890348705"><img src="http://dataskeptic.com/itunes.png" height=35 alt="Data Science itunes" /></a>
-<br/>
+<br/><br/>
 <a href="http://www.stitcher.com/s?fid=50561&refid=stpr"><img src="http://dataskeptic.com/stitcher_234x60.jpg" alt="Data Science Stitcher" height=35></a>
+<br/><br/>
+<a href="http://dataskeptic.com/feed.rss"><img src="http://dataskeptic.com/rss2.gif" /></a>
 </center>
 </div>
 </div>
@@ -43,7 +62,7 @@
 	<h2>The podcast that is skeptical of and with data.</h2>
 	<span class='menu'><a href="/">Home</a></span>
 	|
-	<span class='menu'><a href='/episodes.php'>Show Notes</a></span>
+	<span class='menu'><a href='/episodes.php'>Episodes</a></span>
 	|
 	<span class='menu'><a href='/blog.php'>Blog</a></span>
 	|
