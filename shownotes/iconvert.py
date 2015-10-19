1449dea7bb1a3b38a8ba5bad27f86a1140b6b82d
Kyle Polich <kylepolich@gmail.com>
2015-10-18 17:38:22 -0700

    ipynb show notes

---
+++ shownotes/iconvert.py
@@ -0,0 +1,18 @@
+import BeautifulSoup as soup
+import os
+import sys
+
+file = sys.argv[1]
+f = open(file, 'r')
+data = f.read()
+f.close()
+
+p = soup.BeautifulSoup(data)
+
+middleObj = p.find('div', {'id': 'notebook-container'})
+middle = str(middleObj)
+header = '<? include("../header.php") ?><div id="bbody">'
+footer = '</div><? include("../footer.php") ?>'
+f = open(file[0:file.find('.')] + '.php', 'w')
+f.write(header + middle + footer)
+f.close()
