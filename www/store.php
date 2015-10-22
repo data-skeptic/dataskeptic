a07050900997307ba1851463446d8e06146fa874
Kyle Polich <kyle@datascience.com>
2015-10-22 01:06:57 -0500

    store

---
+++ www/store.php
@@ -0,0 +1,35 @@
+<? include("header.php"); ?>
+
+<div id="bbody">
+
+<h1>Data Skeptic Merch</h1>
+
+<p>All orders include $1 shipping</p>
+
+<p>Sorry, at this time, we can only ship to US addresses.  International listeners are encouraged to announce their displeasure
+with this on social media as a means of motivating the creation of an international shipping option.</p>
+
+<table>
+  <tr>
+    <td><img src=' ' height=300 /></td>
+    <td valign='top'>
+      <h2>Sticker</h2>
+      <p class='price>$1</p>
+    </td>
+  </tr>
+  <tr>
+    <td><img src=' ' height=300 /></td>
+    <td valign='top'>
+      <h2>Pin</h2>
+      <p class='price>$1</p>
+    </td>
+  </tr>
+</table>
+
+<div id='cart'>
+
+</div>
+
+</div>
+
+<? include("footer.php"); ?>
