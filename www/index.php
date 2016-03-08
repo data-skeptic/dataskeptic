540205047c1c3b751b8de10107b1453f591a648d
Kyle Polich <kylepolich@gmail.com>
2016-03-08 08:58:15 -0800

    add cancel notice

--- www/index.php
+++ www/index.php
@@ -35,22 +35,28 @@ Longer interviews feature practitioners and experts on interesting topics relate
 <table>
   <tr>
     <td><i>Title:</i></td>
-    <td>Clustering, beyond k-means</td>
+    <td>THIS EVENT WAS CANCELLED BY THE VENUE :( <strike>Clustering, beyond k-means</strike></td>
   </tr>
   <tr>
     <td><i>Address:</i></td>
-    <td>Silicon Valley Data Science - 150 West Evelyn Avenue #Suite 100, Mountain View, CA 94041</td>
+    <td><strike>Silicon Valley Data Science - 150 West Evelyn Avenue #Suite 100, Mountain View, CA 94041</strike></td>
   </tr>
   <tr>
     <td><i>Datetime:</i></td>
-    <td>6:30pm Friday <b>3/11/2016</b></td>
+    <td><strike>6:30pm Friday <b>3/11/2016</b></strike></td>
   </tr>
   <tr>
     <td></td>
     <td>
+I'm so sorry for the last minute notification.  I was only notified that this event was going to be cancelled after releasing the last episode of
+Data Skeptic, so I was unable to announce the cancellation on the show.  Given the short notice, I won't be able to secure an alternative venue
+so I'm afraid I have to cancel thi Friday talk altogether.  Please come see my Thursday talk with the Bay Area Skeptics if you're available.
+<strike>
+      <br/><br/>
       <a href="https://www.eventbrite.com/e/kyle-polich-svds-clustering-beyond-k-means-registration-22583776684">Please register to get a ticket</a>.
       <br/><br/>
       K-means clustering is known to most data scientists because of it's unsupervised nature and ease of use in tackling clustering problems.  It is widely implemented in many languages, and generally painless to use.  Yet, without proper consideration for the workings of this algorithm, one can easily misunderstand it's output.  Further, there are a variety of other clustering algorithms that offer features k-means does not.  This talk is a survey of many clustering algorithms and a discussion about their proper use.
+</strike>
     </td>
   </tr>
 </table>
