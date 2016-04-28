4bbdeac81804ca609a8847fee0ec479dc14ecd87
Kyle Polich <kyle@datascience.com>
2016-04-28 08:43:27 -0700

    periods

--- www/contact.php
+++ www/contact.php
@@ -6,9 +6,9 @@ Disqus comment area.</p>
 
 <p>For more general feedback, please consider reaching out via Facebook or Twitter so that everyone can benefit from the discussion.</p>
 
-<p>The official twitter account of the show is <a href="https://twitter.com/dataskeptic">@dataskeptic</a></p>
+<p>The official twitter account of the show is <a href="https://twitter.com/dataskeptic">@dataskeptic</a></p>.
 
-<p>Please follow our Facebook page <a href="https://www.facebook.com/dataskeptic">Data Skeptic</a></p>
+<p>Please follow our Facebook page <a href="https://www.facebook.com/dataskeptic">Data Skeptic</a></p>.
 
 <p>I prefer discussion in public places when possible so that every can share in the exchange. However,
 if you want to reach me for something private, you can find my address on my <a href="https://github.com/kylepolich/">github</a> page.</p>
