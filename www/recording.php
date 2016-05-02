b420d88f9572039b0a538967f571d29a8bf52e3a
Kyle Polich <kylepolich@gmail.com>
2016-05-02 07:10:25 -0700

    clean up

---
+++ www/recording.php
@@ -0,0 +1,27 @@
+<? include("header.php"); ?>
+
+<div id="bbody">
+<p>Weclome future Data Skeptic guest!  We are excited to have you coming on the show.</p>
+
+<p>Data Skeptic typically records via Skype, which gives reasonably good call quality if you have
+a decent internet connection.  Kyle will record both sides of the conversation on his end; all you
+need to do is accept the Skype call.</p>
+
+<p>Below are a few bullet points of advice for how you can help achieve the best audio quality.  
+The episodes where my guests can be heard most clearly get noticably more downloads and social 
+media buzz, so please let me know if I can help you achieve the best results.</p>  
+
+<h2>Some steps for achieving good audio quality</h2>
+
+<ul>
+<li>Be in a quiet, issolated room
+<li>Find a room that doesn't have an obvious echo.  Many conference rooms do.
+<li>Check if any machine noise (e.g. refrigerator compressor, computer fan, electronics buzz) could distract from your voice.
+<li>Consider if there might be road / construction noise outside during recording that could leak in through a window.
+<li>If you have access to a "gaming headset", these often have very good audio quality, but even earbugs with a microphone can sound ok.  Your computer's built in microphone will almost certainly sound bad.
+<li>Try the "Skype Call Testing Service" in advance to see how you sound.
+</ul>
+
+</div>
+
+<? include("footer.php"); ?>
