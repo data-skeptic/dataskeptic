042bf588ae6cbd042eb2b95be783a1ba217c65c9
Kyle Polich <kylepolich@gmail.com>
2016-09-08 08:10:44 -0700

    add player to index and new show notes

--- www/index.php
+++ www/index.php
@@ -12,8 +12,12 @@ Longer interviews feature practitioners and experts on interesting topics relate
 <p>Are you a current student studying a field related to data science?  Consider applying for our
 <a href="http://dataskeptic.com/interns">Internship opportunity</a>.</p>
 -->
-
+<table><tr>
+<td>
 <? include("mailinglist.php"); ?>
+</td><td>
+<iframe scrolling="no" frameborder="0" 'style="width:300px;height:180px;border:0;overflow:hidden;" src="http://app.stitcher.com/splayer/f/50561"></iframe>
+</td></tr></table>
 
 </div>
 
