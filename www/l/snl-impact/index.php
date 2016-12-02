f6a54e67022f118efd43ed0d6568ad96f321f17a
Kyle Polich <kylepolich@gmail.com>
2016-12-02 07:43:52 -0800

    snl holder page

---
+++ www/l/snl-impact/index.php
@@ -0,0 +1,22 @@
+<? include("../../header.php"); ?>
+
+<h1>SNL Impact</h1>
+
+<p>Data Skeptic is going to be releasing a series of bonus content related to our recent episode on Causal Impact.</p>
+
+<p>To get started, check out the episode
+<a href="http://dataskeptic.com/epnotes/causal-impact.php">here</a>
+and the Shiny app
+<a href="http://snl.dataskeptic.com/">here</a>.
+</p>
+
+<p>We'll be adding new content to this page in the next two weeks so check back often, or sign up for our mailing list below to get an update when everything has been released.</p>
+
+<div style="margin-left: 100px;">
+<? include("../../mailinglist.php"); ?>
+</div>
+<br/>
+<br/>
+<br/>
+
+<? include("../../footer.php"); ?>
